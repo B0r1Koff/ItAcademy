b5bd4eb6e94fac6e30a39f1b06fe3dd212f420d1
B0r1Koff <vborikov2607@gmail.com>
2023-11-23 21:13:47 +0300

    filter func

--- filter/src/components/Filter/Filter.jsx
+++ filter/src/components/Filter/Filter.jsx
@@ -1,75 +1,55 @@
-import React, { Component } from 'react';
-import './Filter.css'
+import React, { useState } from 'react';
 
-export default class Filter extends Component {
-  constructor(props) {
-    super(props);
+const Filter = ({ items }) => {
+  const [filterText, setFilterText] = useState('');
+  const [sortAlphabetically, setSortAlphabetically] = useState(false);
 
-    this.state = {
-      filterText: '',
-      sortAlphabetically: false,
-    };
+  const handleFilterTextChange = (e) => {
+    setFilterText(e.target.value);
+  };
+
+  const handleSortChange = (e) => {
+    setSortAlphabetically(e.target.checked);
+  };
+
+  const handleReset = () => {
+    setFilterText('');
+    setSortAlphabetically(false);
+  };
+
+  let filteredItems = items.filter((item) =>
+    item.toLowerCase().includes(filterText.toLowerCase())
+  );
+
+  if (sortAlphabetically) {
+    filteredItems = filteredItems.sort();
   }
 
-  handleFilterTextChange = (e) => {
-    this.setState({ filterText: e.target.value });
-  };
-
-  handleSortChange = (e) => {
-    this.setState({ sortAlphabetically: e.target.checked });
-  };
-
-  handleReset = () => {
-    this.setState({
-      filterText: '',
-      sortAlphabetically: false,
-    });
-  };
-
-  render() {
-    const { items } = this.props;
-    const { filterText, sortAlphabetically } = this.state;
-
-    let filteredItems = items.filter((item) =>
-      item.toLowerCase().includes(filterText.toLowerCase())
-    );
-
-    if (sortAlphabetically) {
-      filteredItems = filteredItems.sort();
-    }
-
-    return (
-      <div>
-        <div className='sort-elements'>
-
-          <input
-            className='checkbox-to-sort'
-            type="checkbox"
-            checked={sortAlphabetically}
-            onChange={this.handleSortChange}
-          />
-
-          <input
-            className='input-to-sort'
-            type="text"
-            placeholder="Поиск..."
-            value={filterText}
-            onChange={(e) => {
-              this.handleFilterTextChange(e);
-            }}
-          />
-
-          <button onClick={this.handleReset}>Сброс</button>
-
-        </div>
-
-        <ul className='items-list'>
-          {filteredItems.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-
+  return (
+    <div>
+      <div className='sort-elements'>
+        <input
+          className='checkbox-to-sort'
+          type="checkbox"
+          checked={sortAlphabetically}
+          onChange={handleSortChange}
+        />
+        <input
+          className='input-to-sort'
+          type="text"
+          placeholder="Поиск..."
+          value={filterText}
+          onChange={handleFilterTextChange}
+        />
+        <button onClick={handleReset}>Сброс</button>
       </div>
-    );
-  }
-}
+      <ul className='items-list'>
+        {filteredItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Filter;
